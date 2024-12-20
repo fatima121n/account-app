6fde6a9406b5536c0f3ff1ca6517359f4a636c01
fatima <fatema121n@gmail.com>
2024-12-20 14:26:54 +0430

    Add PasswordResetVerifyView URL for password reset verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . views import PasswordResetRequestView, RegisterUserView
+from . views import PasswordResetRequestView, RegisterUserView, PasswordResetVerifyView
+
 
 
 urlpatterns = [
     path('auth/register/', RegisterUserView.as_view(), name='register'),
     path('auth/password/reset/', PasswordResetRequestView.as_view(), name='password-reset'),
-   
+    path('auth/password/reset/verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify')   
 ]
 
 
