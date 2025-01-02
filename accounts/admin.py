a9213042662a65324b723c80f87bd19f9d5bde61
fatima <fatema121n@gmail.com>
2025-01-02 12:07:58 +0430

    Add UserAdmin configuration for admin interface; display email, staff status, and superuser status with search and filter options.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_staff', 'is_superuser') #Fields to display in the admin list view
+    search_fields = ('email',)
+    list_filter = ('is_staff', 'is_superuser')
